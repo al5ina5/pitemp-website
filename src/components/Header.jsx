ea25fef1887500effe755ad64ebb4b5a4d9d73cf
Sebastian Alsina <alsinas@me.com>
2020-07-10 17:07:07 -0400

    Auto Deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return <>
         <section>
             <nav>
-                <a href="https://www.npmjs.com/package/pitemp" target="_blank">npm on GitHub</a>
+                <a href="https://www.npmjs.com/package/pitemp" target="_blank">pitemp on npm</a>
                 <a href="https://www.github.com/al5ina5/pitemp" target="_blank">pitemp on GitHub</a>
             </nav>
         </section>
