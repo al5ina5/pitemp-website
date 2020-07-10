a8eb3f72ed6e6ef8a8766d7094f2015e5d773f37
play.js <git@gituser.com>
2020-07-10 17:17:02 -0400

    Error

--- src/components/Meta.jsx
+++ src/components/Meta.jsx
@@ -5,12 +5,12 @@ const Meta = () => (
         <title>pitemp by Sebatian Alsina</title>
         <meta name="description" content="A command-line utility to get the temperature of your machine." />
         <meta property="og:type" content="website" />
-        <meta name="og:title" property="og:title" content="pitemp by Sebatian Alsina" />
+        <meta name="og:title" property="og:title" content="pitemp by Sebastian Alsina" />
         <meta name="og:description" property="og:description" content="A command-line utility to get the temperature of your machine." />
-        <meta property="og:site_name" content="pitemp by Sebatian Alsina" />
+        <meta property="og:site_name" content="pitemp by Sebastian Alsina" />
         <meta property="og:url" content="https://revolt.host" />
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content="pitemp by Sebatian Alsina" />
+        <meta name="twitter:title" content="pitemp by Sebastian Alsina" />
         <meta name="twitter:description" content="A command-line utility to get the temperature of your machine." />
         <meta name="twitter:site" content="https://revolt.host" />
         <meta name="twitter:creator" content="@revolthost" />
