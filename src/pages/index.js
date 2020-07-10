898e912d68fb5e0b38c3175538687b8e546b9448
Sebastian Alsina <alsinas@me.com>
2020-07-10 17:26:22 -0400

    Auto Deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 export default function Index() {
     return <>
-
         <section>
             <h1><code>pitemp</code></h1>
             <h2>Quickly get the temperature of your machine.</h2>
@@ -8,7 +7,7 @@ export default function Index() {
                 A quick and easy way to get the temperature of your Raspberry Pi with a single command. Works on MacOS + Windows. Built in NodeJS.
             </p>
             <p>
-                <a href="https://www.npmjs.com/package/pitemp">Learn how to use <code>pitemp</code> by reading the documentation.</a>
+                <a target="_blank" href="https://www.npmjs.com/package/pitemp">Learn how to use <code>pitemp</code> by reading the documentation.</a>
             </p>
         </section>
 
@@ -20,7 +19,7 @@ export default function Index() {
             <h1>Support <code>pitemp</code></h1>
             <p>If you'd like to support <code>pitemp</code> because you found it helpful, clever, ingenious, or maybe because you absolutely hate it, you'd be contributing to something great!</p>
             <h3>Patreon</h3>
-            <a href="https://patreon.com/sebastianalsina">
+            <a target="_blank" href="https://patreon.com/sebastianalsina">
                 <button>
                     Become a Patron
                     </button>
@@ -29,6 +28,11 @@ export default function Index() {
             <pre>3AJ6FtwZpvP6UExcPqnG4hWgivvAxf8ejf</pre>
             <h3>Stellar Address</h3>
             <pre>GDY6TJFWJTEPY6UPYTE7756I3LAY34FODNJET3TJPNJKN34UMS4PGVM7</pre>
+        </section>
+
+        <section>
+            <p><code>pitemp</code> &copy; {new Date().getFullYear()}</p>
+            <p>Website styled using <a target="_blank" href="http://anticss.vercel.app">anti.css by Sebastian Alsina</a>.</p>
         </section>
     </>
 }
